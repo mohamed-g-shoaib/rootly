869e8fb8b225155b57ba7e1fde51bf27d6ade9b9
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2026-03-10 07:30:17 +0200

    chore: add all ui components and install remaining dependencies needed by rootly

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { AnchoredToastProvider, ToastProvider } from "@/components/ui/toast"
+
 import { cn } from "@/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
@@ -23,7 +25,13 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <ToastProvider>
+            <AnchoredToastProvider>
+              {children}
+            </AnchoredToastProvider>
+          </ToastProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
